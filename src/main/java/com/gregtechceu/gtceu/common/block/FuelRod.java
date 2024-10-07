9c7ca5d4d20ffcc040fcdd6d8670c077354feff1
sqcode06 <sqcode06@gmail.com>
2024-10-08 00:41:06 +0300

    FuelRod is now waterloggable

--- src/main/java/com/gregtechceu/gtceu/common/block/FuelRod.java
+++ src/main/java/com/gregtechceu/gtceu/common/block/FuelRod.java
@@ -9,17 +9,24 @@ import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
 import lombok.Getter;
 import lombok.NonNull;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class FuelRod extends AppearanceBlock implements IReactorFuelRod {
+public class FuelRod extends AppearanceBlock implements IReactorFuelRod, SimpleWaterloggedBlock {
     @Persisted
     @Getter
     public double heat;
@@ -27,14 +34,14 @@ public class FuelRod extends AppearanceBlock implements IReactorFuelRod {
     private IFissionReactor reactor;
 
     public static final EnumProperty<VLinkTypes> V_LINK = EnumProperty.create("vlink", VLinkTypes.class);
-
     public static final IntegerProperty RODS = IntegerProperty.create("rods", 0, 0xFF);
 
     public FuelRod(Properties properties) {
         super(properties);
         registerDefaultState(defaultBlockState()
                 .setValue(V_LINK, VLinkTypes.NONE)
-                .setValue(RODS, 0b11_10_01_00));
+                .setValue(RODS, 0)
+                .setValue(BlockStateProperties.WATERLOGGED, false));
     }
 
 
@@ -63,14 +70,31 @@ public class FuelRod extends AppearanceBlock implements IReactorFuelRod {
     }
 
     @Override
-    public void setReactor(IFissionReactor reactor)
+    public void setReactor(@Nullable IFissionReactor reactor)
     {
         this.reactor = reactor;
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        super.createBlockStateDefinition(builder.add(V_LINK, RODS));
+        super.createBlockStateDefinition(builder.add(V_LINK, RODS, BlockStateProperties.WATERLOGGED));
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public @NotNull FluidState getFluidState(BlockState state) {
+        return state.getValue(BlockStateProperties.WATERLOGGED) ? Fluids.WATER.getSource(false) :
+                super.getFluidState(state);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level,
+                                  BlockPos pos, BlockPos neighborPos) {
+        if (state.getValue(BlockStateProperties.WATERLOGGED)) {
+            level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        }
+        return super.updateShape(state, direction, neighborState, level, pos, neighborPos);
     }
 
     @Override
@@ -94,15 +118,12 @@ public class FuelRod extends AppearanceBlock implements IReactorFuelRod {
     private void updateVLinkState(BlockState state, Level level, BlockPos pos) {
         int hasBlockAbove = (level.getBlockState(pos.above()).getBlock() instanceof IReactorFuelConnector) ? 1 : 0;
         int hasBlockBelow = (level.getBlockState(pos.below()).getBlock() instanceof IReactorFuelConnector) ? 1 : 0;
-        VLinkTypes type =  switch (hasBlockAbove<<1 | hasBlockBelow) {
+        VLinkTypes type = switch (hasBlockAbove << 1 | hasBlockBelow) {
             case 0b01 -> VLinkTypes.DOWN;
             case 0b10 -> VLinkTypes.UP;
             case 0b11 -> VLinkTypes.BOTH;
             default -> VLinkTypes.NONE;
         };
-        //if(state.getValue(RODS)==0b11_10_01_00){
-        //    level.setBlock(pos, state.setValue(V_LINK, type).setValue(RODS, level.random.nextInt(0, 0xFF)), 3);
-        //}
         level.setBlock(pos, state.setValue(V_LINK, type), 3);
     }
 
